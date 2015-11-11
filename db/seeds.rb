4faea34de8741157e1c422c8deaf04065ed5ea9f
Justin Pease <justinpease2@gmail.com>
2015-11-11 16:09:33 -0700

    nothing really to commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require_relative 'seeds/users'
+require_relative 'seeds/sales'
