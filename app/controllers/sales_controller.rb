dcfadca034f386a57fb031f429e7c13e9e508664
Justin Pease <justinpease2@gmail.com>
2015-11-12 16:02:51 -0700

    sales person can edit a sale

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,11 +1,34 @@
 class SalesController < ApplicationController
 
   def index
-    # @sales = Sale.where({user_id: session[:user_id], :created_at => 1.months.ago..Time.now})#.order(created_at: :desc)
-    # @sales = Sale.where({user_id: session[:user_id], created_at: strftime('%m', created_at) = ?, Time.now.month)})
     @sales = Sale.where('extract(month  from created_at) = ? and user_id = ?', Time.now.month, session[:user_id])
-    # byebug
+  end
+
+  def edit
+    @sale = Sale.find(params[:id])
+  end
+
+  def update
+    sale = Sale.find(params[:id])
+    if sale.update_attributes(sale_params)
+      redirect_to sales_path
+    else
+      render "edit"
+    end
+  end
+
+
+
+  private
+
+  def sale_params
+    params.require(:sale).permit( :user_id, :pages, :quantity, :job_title,
+                                  :job_title, :amount, :customer, :mark_up,
+                                  :frequency, :single_sale)
+  end
+
+  def sale_month
+    month = params[:month]
+    Time.now
   end
 end
-
-# Sale.where(["user_id = ? and created_at = ?", session[:user_id], ])
