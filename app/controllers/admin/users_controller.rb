ec3cbee2f13a381f69bb8adb06f85cd8682ef37a
Justin Pease <justinpease2@gmail.com>
2015-11-17 00:16:48 -0700

    adds some stuff in admin controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,17 +7,27 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def index
+    @salespeople = User.where(role: 1)
   end
 
   def show
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+    user.update_attributes(update_user_params)
+    redirect_to admin_users_path
   end
 
   def delete
+  end
+
+  private
+  def update_user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
