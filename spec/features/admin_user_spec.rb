8d873956602d027a3ce4846de9e0a68af87d817e
Justin Pease <justinpease2@gmail.com>
2015-11-23 14:06:12 -0700

    fixed some styling issues

--- spec/features/admin_user_spec.rb
+++ spec/features/admin_user_spec.rb
@@ -4,10 +4,17 @@ describe 'the login page', type: :feature do
   describe "as an admin loggin in" do
     it "loggs me in and takes me to the company sales summary page" do
       visit '/'
-      ("User Name", with: "Justin")
+      # select(value='Justin', :from => 'user[name]')
+      # within 'user[name]' do
+      #   find("option[value='Justin']").click
+      # end
+      # find_field("user_name").select("Justin").click
+      within "#names" do
+        find('value[Justin]', text: 'Justin').select_option
+      end
+      fill_in(password)
       page.fill_in("Password", with: "password")
-      click_button("Login")
-      expect(page).to have_content("Welcome Sales Person")
+      expect(page).to have_content("Justin")
     end
   end
 end
